36404db8229023e33b3efbf42ca5a5bd574e8cc1
PepeLopez7 <101602855+PepeLopez7@users.noreply.github.com>
2022-04-20 18:25:56 +0200

    Implementando

--- JuegosJava/src/Main.java
+++ JuegosJava/src/Main.java
@@ -2,7 +2,13 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		System.out.println("Elige uno de los siguientes juegos: ");
+		
+		System.out.println("Suma dos números");
+		System.out.println("Juego 2");
+		System.out.println("Adivina un número entre 1 y 100");
 
 	}
 
