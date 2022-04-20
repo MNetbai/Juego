1471fd74bf8bdf44a5a57addbed8e610669296fe
PepeLopez7 <101602855+PepeLopez7@users.noreply.github.com>
2022-04-20 18:17:11 +0200

    Juego1 commit

--- JuegosJava/src/Juego1.java
+++ JuegosJava/src/Juego1.java
@@ -1,4 +1,32 @@
+import java.util.Scanner;
 
 public class Juego1 {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("'SUMA DOS NÚMEROS'");
+	
+	    Scanner sc = new Scanner(System.in);
+	
+	    int n1 = 0;
+	    int n2 = 0;
+	    int resultado;
+	
+	    System.out.println("Introduce un número entero: ");
+	    if (sc.hasNextInt()) {
+	        n1 = sc.nextInt();
+	        sc.nextLine();
+	    }
+	
+	    System.out.println("Introduce otro número entero: ");
+	    if (sc.hasNextInt()) {
+	        n2 = sc.nextInt();
+	        sc.nextLine();
+	    }
+	    resultado = n1 + n2;
+	
+	    System.out.println("Tu resultado es: " + resultado);
+    
+	}
 
 }
