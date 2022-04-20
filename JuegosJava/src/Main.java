8356783eac5fe323fa95d6e662ee61725c78350e
PepeLopez7 <101602855+PepeLopez7@users.noreply.github.com>
2022-04-20 18:35:40 +0200

    Implementando

--- JuegosJava/src/Main.java
+++ JuegosJava/src/Main.java
@@ -8,7 +8,9 @@ public class Main {
 		System.out.println("Suma dos números");
 		Juego1 juego1 = new Juego1();
 		System.out.println("Juego 2");
+		Juego2 juego2 = new Juego2();
 		System.out.println("Adivina un número entre 1 y 100");
+		Juego3 juego3 = new Juego3();
 
 	}
 
