f04713e9ddf94c37e447badca8a4782e52531082
PepeLopez7 <101602855+PepeLopez7@users.noreply.github.com>
2022-04-21 16:52:25 +0200

    Final

--- JuegosJava/src/Juego1.java
+++ JuegosJava/src/Juego1.java
@@ -4,28 +4,30 @@ public class Juego1 {
 	
 	public Juego1() {
 	
-	    System.out.println("'SUMA DOS NÃšMEROS'");
+	    System.out.println("'ACIERTA EL RESULTADO'");
 	
 	    Scanner sc = new Scanner(System.in);
 	
-	    int n1 = 0;
-	    int n2 = 0;
-	    int resultado;
-	
-	    System.out.println("Introduce un número entero: ");
-	    if (sc.hasNextInt()) {
-	        n1 = sc.nextInt();
-	        sc.nextLine();
-	    }
-	
-	    System.out.println("Introduce otro nÃºmero entero: ");
-	    if (sc.hasNextInt()) {
-	        n2 = sc.nextInt();
-	        sc.nextLine();
-	    }
+	    int n1 = (int) (Math.random() * 100);
+	    int n2 = (int) (Math.random() * 100);
+	    int resultado = 0;
+	    int miResultado = 0;
+	    
 	    resultado = n1 + n2;
 	
-	    System.out.println("Tu resultado es: " + resultado);
+	    System.out.println(n1 + " + " + n2 + " = ?");
+	    
+	    System.out.println("Introduce el resultado: ");
+	    if (sc.hasNextInt()) {
+	        miResultado = sc.nextInt();
+	        sc.nextLine();
+	    }
+	    
+	    if (miResultado == resultado) {
+	    	System.out.println("¡Acierto!, el resultado es " + resultado);
+	    } else {
+	    	System.out.println("¡Error!, el resultado es " + resultado);
+	    }
     
 	}
 
