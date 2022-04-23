0dbd4cd65fa783c8760b06f6292bc3cec3b08faa
Junglajaime <jaimerodriguezbenitez@iestorredelrey.es>
2022-04-23 11:53:50 +0200

    Meto el el segundo juego y doy opcion de salir en el switch

--- JuegosJava/src/Juego2.java
+++ JuegosJava/src/Juego2.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
 
 public class Juego2 {
 	
 	public Juego2() {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		int aleatorio = (int) (Math.random()*2);
+		int eleccion= -1;
+		do {
+			System.out.println("Escoge, Cara (0) o Cruz (1)");
+			eleccion = sc.nextInt();
+		}while(eleccion<0 && eleccion>1);
+		
+		System.out.println("............. Tirando la moneda .....................");
+		
+		if(eleccion == aleatorio) {
+			System.out.println("Has tenido mucha suerte ,ha salido ");
+			if(aleatorio == 0) {
+				System.out.printf("Cara");
+			}else System.out.printf("Cruz");
+		}else {
+			System.out.printf("No es lo tuyo esto de tirar monedas...");
+		}
+		
+		
 		
 		
 		
